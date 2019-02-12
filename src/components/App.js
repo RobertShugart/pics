5b8af52c618c6c620a817b5939c5e9075de95d50
RobertS <rshugart@protonmail.com>
2019-02-12 13:00:34 -0500

    Added in the Unsplash API and got a response

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  async onSearchSubmit(term) {
+    const response = axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          " Client-ID fd8126bf4f1e25833a7b14b37162985b4994e663ec1bbc814f7b1fc56aadc960"
+      }
+    });
+
+    console.log(response.data.results);
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
